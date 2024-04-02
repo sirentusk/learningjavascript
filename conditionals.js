3747efcc663d6ab52189d89e49406a7c9e9a46a6
Siren <143248933+sirentusk@users.noreply.github.com>
2024-04-02 14:58:05 +1100

    Update conditionals.js

--- conditionals.js
+++ conditionals.js
@@ -13,3 +13,32 @@ do ... while
 
 ​​​​forEach
 Math
+
+--
+
+// IF //
+
+// Check book was written before the year 2000 //
+let number1 = book1.year;
+let number2 = 2000;
+if (number1 < number2) {    
+    console.log(`${book1.title} was published before the year 2000.`);
+} else {   
+    console.log(`${book1.title} was published after the year 2000.`);
+}
+
+// Check book was written after the year 2000 //
+let number3 = book1.year;
+let number4 = 1930;
+if (number3 < number4) {    
+    console.log(`${book1.title} was published before the year 1930.`);
+} else {   
+    console.log(`${book1.title} was published after the year 1930.`);
+}
+
+// FOR //
+
+
+// WHILE //
+
+
