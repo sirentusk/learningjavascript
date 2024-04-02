9ffadffa830b810e3dec19e83ea823c744a1b6f9
Siren <143248933+sirentusk@users.noreply.github.com>
2024-04-02 14:05:37 +1100

    Create name.js

---
+++ name.js
@@ -0,0 +1,13 @@
+function printGreeting(name) {
+    console.log(`Welcome to my walkthrough. My name is ${name}`);
+}
+
+printGreeting("Sireno" + ".");
+
+function printGreeting(name) {
+    console.log(`Welcome to my walkthrough. My name's ${name}`);
+}
+
+const firstName = "Siren";
+let myName = firstName + " " + "Watcher";
+printGreeting(myName);
